c4a6ba960c50dd7b942efa9e2aa068f35f1c5158
Greeshma <greeshmatm.675@gmail.com>
2022-08-19 16:47:33 +0530

    first commit

---
+++ src/app/dummy.js
@@ -0,0 +1,17 @@
+var dummy=[
+    {
+        progress:"14",
+        progress_per:"54%",
+        data:"Number of tasks on track"
+    },
+    {
+        progress:"14",
+        progress_per:"54%",
+        data:"Number of tasks on track"
+    },
+    {
+        progress:"14",
+        progress_per:"54%",
+        data:"Number of tasks on track"
+    }
+]
